6104491264e4cfe087b4d9397063f4e708f34f2f
Hirohsi <36098150+Hirohsi@users.noreply.github.com>
2018-02-03 18:08:24 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,484 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ひろほしのオフィシャルサイト</title>
+<meta name="keywords" content="ひろほし,Hirohsi,まつ,ヒロ,Minecraft">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="ひろほしのオフィシャルサイトです。">
+<link rel="shortcut icon" href="favicon.ico">
+
+
+
+<!-- CSS
+================================================== -->
+
+
+<!-- Fontawesome Icon font -->
+<link rel="stylesheet" href="css/font-awesome.min.css">
+<!-- bootstrap.min -->
+<link rel="stylesheet" href="css/jquery.fancybox.css">
+<!-- bootstrap.min -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<!-- bootstrap.min -->
+<link rel="stylesheet" href="css/owl.carousel.css">
+<!-- bootstrap.min -->
+<link rel="stylesheet" href="css/slit-slider.css">
+<!-- bootstrap.min -->
+<link rel="stylesheet" href="css/animate.css">
+<!-- Main Stylesheet -->
+<link rel="stylesheet" href="css/main.css">
+
+<!-- Modernizer Script for old Browsers -->
+<script src="js/modernizr-2.6.2.min.js"></script>
+
+</head>
+
+<body id="body">
+
+
+
+<!--
+Fixed Navigation
+==================================== -->
+<header id="navigation" class="navbar-inverse navbar-fixed-top animated-header">
+<div class="container">
+<div class="navbar-header">
+<!-- responsive nav button -->
+<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+<span class="sr-only"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+
+</button>
+<!-- /responsive nav button -->
+
+<!-- logo -->
+<h1 class="navbar-brand">
+<a href="https://hiro84.tokyo/">ひろほしのオフィシャルサイト</a>
+</h1>
+<!-- /logo -->
+</div>
+
+<!-- main nav -->
+<nav class="collapse navbar-collapse navbar-right" role="navigation">
+<ul id="nav" class="nav navbar-nav">
+<li><a href="#body">トップ</a></li>
+<li><a href="#yotei">予定表</a></li>
+<li><a href="#niconico">ニコニコ</a></li>
+<li><a href="#youtube">ユーチューブ</a></li>
+<li><a href="#Link">リンク</a></li>
+<li><a href="#contact">お問い合わせ</a></li>
+</ul>
+</nav>
+<!-- /main nav -->
+
+</div>
+</header>
+<!--
+End Fixed Navigation
+==================================== -->
+
+<main class="site-content" role="main">
+
+<!--
+Home Slider
+==================================== -->
+
+<section id="home-slider">
+<div id="slider" class="sl-slider-wrapper">
+
+<div class="sl-slider">
+
+<div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="-25" data-slice2-rotation="-25" data-slice1-scale="2" data-slice2-scale="2">
+
+<div class="bg-img bg-img-2"></div>
+
+<div class="slide-caption">
+<div class="caption-content">
+<h2 class="animated fadeInDown"><img src="img/Hiro_logo.png" width="500" height="277"></h2>
+
+
+</div>
+</div>
+
+</div>
+
+
+
+<nav id="nav-dots" class="nav-dots visible-xs visible-sm hidden-md hidden-lg">
+
+</nav>
+
+</div><!-- /slider-wrapper -->
+</section>
+
+<!--
+End Home SliderEnd
+==================================== -->
+
+<!-- about section -->
+<section id="about" >
+<div class="container">
+<div class="row">
+<div class="col-md-4 wow animated fadeInLeft">
+<div class="recent-works">
+<h3>自己紹介</h3>
+<div id="works">
+<div class="work-item">
+<p><b>名前：</b>ひろほし<br><b>生年月日：</b>12月9日<br><b>趣味：</b>動画制作・ゲーム<br><b>好きなゲーム:</b>Minecraft / ポケモン<br><b>好きなこと：</b>遊園地 / 旅<br><b>好きなアニメ：</b>ポケモン / 不二子作品 / ドラゴンボール</p>
+</div>
+
+</div>
+</div>
+</div>
+<div class="col-md-7 col-md-offset-1 wow animated fadeInRight">
+<div class="welcome-block">
+<h3>ひろほしの名前の由来</h3>
+  <div class="message-body">
+<img src="img/member-1.jpg" class="pull-left" alt="member">
+<p><br>元々名前は「博（ひろし）」だったのですが、MinecraftのID。「Hiroshi」を間違えて「Hirohsi」と打ってしまって名前を変えました。<br>ネットでの名前は変えたほうがいいかなと思い「ひろほし」で活動してます！</p>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+</section>
+<!-- end about section -->
+
+
+
+<section id="yotei">
+<div class="container">
+<div class="row">
+
+<div class="sec-title text-center">
+<h2 class="wow animated bounceInLeft"><i class="fa fa-calendar fa-1x"></i> 予定表</h2>
+<p class="wow animated bounceInRight"><a href="calendar/admin.php">Calendar</a></p>
+</div>
+
+<p class="text-center">
+ひろほしの生活においての予定表です。
+<br>
+予定が変わる場合もあります。（毎週火曜日更新）<br>
+<br>
+<iframe width="640" height="480" src="calendar" frameborder="0"></iframe>
+</p>
+</div>
+</div>
+</section>
+
+
+
+
+<section id="niconico">
+<div class="container">
+<div class="row">
+
+<div class="sec-title text-center">
+<h2 class="wow animated bounceInLeft"><i class="fa fa-bolt fa-1x"></i> ニコニコ</h2>
+<p class="wow animated bounceInRight">niconico</p>
+</div>
+
+
+
+
+<p class="text-center">
+ニコニコ動画に動画をアップロードしたりしています！<br>
+踊ってみたやマインクラフトの動画などを主に上げております！<br>
+<br>
+ニコニコ生放送では主にゲーム配信をしてます。時々意味不明なことも。<br>
+<br>
+</div>
+</div>
+</section>
+
+
+<section id="youtube">
+<div class="container">
+<div class="row">
+
+<div class="sec-title text-center">
+<h2 class="wow animated bounceInLeft"><i class="fa fa-youtube fa-1x"></i> ユーチューブ</h2>
+<p class="wow animated bounceInRight">YouTube</p>
+</div>
+
+<p class="text-center">
+YouTubeに動画もアップしてます！！<br>
+<b>ゲーム実況</b>や<b>思い出動画</b>や<b>エンターテインメント</b>などを主にアップしてます！<br>
+<br>
+<script src="https://apis.google.com/js/platform.js"></script>
+<div class="col-md-4 wow animated fadeInUp" data-wow-delay="0.2s">
+<div class="price-table text-center">
+<span>メインちゃんねる</span>
+<div class="value">
+<span><div class="g-ytsubscribe" data-channelid="UCv-_nMaHVsfAutOgx5xKwmw" data-layout="full" data-count="default"></div></span><br>
+</div>
+</div>
+</div>
+
+<div class="col-md-4 wow animated fadeInUp" data-wow-delay="0.4s">
+<div class="price-table text-center">
+<span>ゲームちゃんねる</span>
+<div class="value">
+<span><div class="g-ytsubscribe" data-channelid="UC4ZFF3FJ-eE6AKfY9wh9mWA" data-layout="full" data-count="default"></div></span><br>
+</div>
+</div>
+</div>
+
+<div class="col-md-4 wow animated fadeInUp" data-wow-delay="0.6s">
+<div class="price-table text-center">
+<span>不思議の探検家ちゃんねる</span>
+<div class="value">
+<span><div class="g-ytsubscribe" data-channelid="UCJkrorFKKnlpfiZlWcoaH1w" data-layout="full" data-count="default"></div></span><br>
+</div>
+</div>
+</div>
+
+
+
+<br>
+
+</p>
+</div>
+</div>
+</section>
+
+
+
+<!-- Testimonial section -->
+<section id="testimonials" class="parallax">
+<div class="overlay">
+<div class="container">
+<div class="row">
+
+<div class="sec-title text-center white wow animated fadeInDown">
+<h2>ひろほしを詳しく</h2>
+</div>
+
+<div id="testimonial" class=" wow animated fadeInUp">
+<div class="testimonial-item text-center">
+<img src="img/member-1.jpg" alt="Our Clients">
+<div class="clearfix">
+<span>ひろほしについて</span>
+<p>実は一時期フィリピンで育ちました。幼稚園でフィリピン語の教育を受けて時々日本の幼稚園にも行ったりと、何を勉強したらいいのかわからなくなり、変な人になってしまいました。
+小学２年生から日本に住んでますが、日本語が時々おかしい時があるので暖かい気持ちで聞いてください。</p>
+</div>
+</div>
+<div class="testimonial-item text-center">
+<img src="img/member-1.jpg" alt="Our Clients">
+<div class="clearfix">
+<span>主な活動</span>
+<p>ニコニコでは、踊ってみた・歌ってみた（下手くそ）・マインクラフト動画・イベント企画・生放送などやってます！<br>
+YouTubeでは、エンターテインメント・ゲーム実況・思い出動画を不定期にアップロードしています！</p>
+</div>
+</div>
+
+
+</div>
+</div>
+</div>
+
+</section>
+<!-- end Testimonial section -->
+
+<!-- Price section -->
+<section id="Link">
+<div class="container">
+<div class="row">
+
+<div class="sec-title text-center wow animated fadeInDown">
+<h2>リンク</h2>
+<p>Link</p>
+</div>
+
+<div class="col-md-4 wow animated fadeInUp" data-wow-delay="0.2s">
+<div class="price-table text-center">
+<span>ひろほしプロジェクト</span>
+<div class="value">
+<span>以下のリンク</span><br>
+</div>
+<ul>
+<li><a href="McProject">McProject</a></li>
+<li><a href="DeathKanransha">Death観覧車</a></li>
+<li></li>
+</ul>
+</div>
+</div>
+
+<div class="col-md-4 wow animated fadeInUp" data-wow-delay="0.4s">
+<div class="price-table text-center">
+<span>マインクラフトツール</span>
+<div class="value">
+<span>以下のリンク</span><br>
+</div>
+<ul>
+<li><a href="MinecraftTool/skinget/">スキン取得</a></li>
+<li>サーバー情報取得</li>
+<li></li>
+</ul>
+</div>
+</div>
+
+<div class="col-md-4 wow animated fadeInUp" data-wow-delay="0.8s">
+<div class="price-table text-center">
+<span>その他</span>
+<div class="value">
+<span>以下のリンク</span><br>
+</div>
+<ul>
+<li><a href="Downloader">ダウンローダー</a></li>
+<li><a href="diary">ひろほしにっき</a></li>
+<li></li>
+</ul>
+</div>
+</div>
+</div>
+<br><br>
+<div class="wow animated fadeInUp" data-wow-delay="1.2s">
+<table class="table">
+<tbody>
+<tr>
+<th>今日の日付</th>
+<td><?php echo date("n月j日 H時i分");?></td>
+</tr>
+<tr>
+<th>あなたのIPアドレス</th>
+<td><?php echo $_SERVER["REMOTE_ADDR"];?></td>
+</tr>
+<tr>
+<th>あなたのホストネーム</th>
+<td><?php echo $_SERVER['SERVER_NAME'];?></td>
+</tr>
+
+</tbody>
+</table>
+
+</div>
+</div>
+</section>
+<!-- end Price section -->
+
+<!-- Social section -->
+<section id="social" class="parallax">
+<div class="overlay">
+<div class="container">
+<div class="row">
+
+<div class="sec-title text-center white wow animated fadeInDown">
+<h2>FOLLOW US</h2>
+<p>繋がろう。</p>
+</div>
+
+<ul class="social-button">
+<li class="wow animated zoomIn"><a href="https://twitter.com/pa_mineburo"><i class="fa fa-twitter fa-2x"></i></a></li>
+<li class="wow animated zoomIn" data-wow-delay="0.3s"><a href="https://www.youtube.com/channel/UCv-_nMaHVsfAutOgx5xKwmw"><i class="fa fa-youtube fa-2x"></i></a></li>
+<li class="wow animated zoomIn" data-wow-delay="0.6s"><a href="https://www.instagram.com/pa_mineburo/"><i class="fa fa-instagram fa-2x"></i></a></li>
+</ul>
+
+</div>
+</div>
+</div>
+</section>
+<!-- end Social section -->
+
+<!-- Contact section -->
+<section id="contact" >
+<div class="container">
+<div class="row">
+
+<div class="sec-title text-center wow animated fadeInDown">
+<h2>Contact</h2>
+<p><s>お問い合わせ</s><br>ごめんまだ使えない</p>
+</div>
+
+
+<div class="col-md-7 contact-form wow animated fadeInLeft">
+<form action="send.php" method="post">
+<div class="col-md-12">
+<div class="form-group">
+<label for="name" class="sr-only">Name</label>
+<input placeholder="Name" name="name" type="text" class="form-control input-lg">
+</div>
+</div>
+<div class="col-md-12">
+<div class="form-group">
+<label for="email" class="sr-only">Email</label>
+<input placeholder="Email" name="mail" type="text" class="form-control input-lg">
+</div>
+</div>
+
+<div class="col-md-12">
+<div class="form-group">
+<label for="message" class="sr-only">Message</label>
+<textarea placeholder="Message" name="body" class="form-control input-lg" rows="3"></textarea>
+</div>
+</div>
+<div class="col-md-12">
+<div class="form-group">
+<input type="submit" class="btn btn-primary btn-lg " value="Send">
+
+</div>
+</div>
+
+
+</form>
+
+</div>
+
+<div class="col-md-5 wow animated fadeInRight">
+<address class="contact-details">
+<h3>Contact Us</h3>
+<p><i class="fa fa-pencil"></i>名前のところはニックネームで構いません！</span><span>メールアドレスを必ず確認してください！</p><br>
+<p><i class="fa fa-envelope"></i>pa_mineburo@yahoo.co.jp</p>
+</address>
+</div>
+
+</div>
+</div>
+</section>
+<!-- end Contact section -->
+
+
+</main>
+
+<footer id="footer">
+<div class="container">
+<div class="row text-center">
+<div class="footer-content">
+<div class="wow animated fadeInDown">
+<p>ひろほしのオフィシャルサイト</p><br>
+</div>
+
+
+<p>Copyright &copy; 2018 hiro84.tokyo All rights reserved.</p>
+</div>
+</div>
+</div>
+</footer>
+
+<!-- Essential jQuery Plugins
+================================================== -->
+<!-- Main jQuery -->
+<script src="js/jquery-1.11.1.min.js"></script>
+<!-- Twitter Bootstrap -->
+<script src="js/bootstrap.min.js"></script>
+<!-- Single Page Nav -->
+<script src="js/jquery.singlePageNav.min.js"></script>
+<!-- jquery.fancybox.pack -->
+<script src="js/jquery.fancybox.pack.js"></script>
+
+<!-- Owl Carousel -->
+<script src="js/owl.carousel.min.js"></script>
+<!-- jquery easing -->
+<script src="js/jquery.easing.min.js"></script>
+<!-- Fullscreen slider -->
+<script src="js/jquery.slitslider.js"></script>
+<script src="js/jquery.ba-cond.min.js"></script>
+<!-- onscroll animation -->
+<script src="js/wow.min.js"></script>
+<!-- Custom Functions -->
+<script src="js/main.js"></script>
+</body>
+</html>
